f2baf486ab42152b26de55f6bf116aac721445b6
ikerove <52752754+ikerove@users.noreply.github.com>
2020-11-25 17:17:44 +0100

    creada ventana principal(main)

---
+++ Videoclub/src/ventanas/VentanaMain.java
@@ -0,0 +1,18 @@
+package ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class VentanaMain {
+	private JPanel panCentro ;
+	private JScrollPane panScrol;
+	private JMenuBar menuBar;
+	private JMenu menuS, menuP, menuD;
+	private JMenuItem mi1, mi2, mi3;
+	private JFrame v;
+
+}
