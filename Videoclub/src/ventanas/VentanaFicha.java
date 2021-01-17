150199a1a4d390feb76a2548fbf939e10217eb16
ikerove <52752754+ikerove@users.noreply.github.com>
2021-01-17 17:38:44 +0100

    nada

--- Videoclub/src/ventanas/VentanaFicha.java
+++ Videoclub/src/ventanas/VentanaFicha.java
@@ -102,6 +102,7 @@ public class VentanaFicha extends JFrame{
 				new VentanaThreadPago();*/
 			}
 		});
+		
 		btnCarrito = new JButton("Añadir al carrito");
 		btnCarrito.addActionListener(new ActionListener() {
 			
